cb60ab8d2ca5e0d967c49d38f8a90fe3b3b7e6a3
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 18:06:38 -0500

    testing 2

--- api/task/router.js
+++ api/task/router.js
@@ -18,36 +18,6 @@ router.get("/", (req, res) => {
         })
 });
 
-// router.get("/", (req, res) => {
-//   const toReturn = [];
-//   Model.getAll().then((data) => {
-//     for (let i in data) {
-//       if (data[i].completed === 1) {
-//         toReturn.push({
-//           id: data[i].id,
-//           project_id: data[i].project_id,
-//           description: data[i].description,
-//           notes: data[i].notes,
-//           completed: true,
-//           project_name: data[i].project_name,
-//           project_description: data[i].project_description,
-//         });
-//       } else {
-//         toReturn.push({
-//           id: data[i].id,
-//           project_id: data[i].project_id,
-//           description: data[i].description,
-//           notes: data[i].notes,
-//           completed: false,
-//           project_name: data[i].project_name,
-//           project_description: data[i].project_description,
-//         });
-//       }
-//     }
-//     res.status(200).json(toReturn);
-//   });
-// });
-
 router.post("/", (req, res) => {
   Model.create(req.body)
     .then((data) => {
@@ -81,5 +51,33 @@ router.post("/", (req, res) => {
       res.status(400).json(err.message);
     });
 });
+
+// router.post("/", (req, res) => {
+//   Model.create(req.body)
+//              .then((data) => {
+//                         return Model.getById(data);
+//                     })
+//               .then((data) => {
+//                     if (data["completed"] === 0) 
+//                     {
+//                         res.status(201).json({
+//                         id: data.id,
+//                         name: data.name,
+//                         description: data.description,
+//                         completed: false,
+//                         });
+//                     } else {
+//                         res.status(200).json({
+//                         id: data.id,
+//                         name: data.name,
+//                         description: data.description,
+//                         completed: true,
+//                         });
+//                     }
+//                  })     
+//             .catch(error=>{
+//                 res.status(500).json({errorMessage: error.message})
+//             })
+// });
 
 module.exports = router;
