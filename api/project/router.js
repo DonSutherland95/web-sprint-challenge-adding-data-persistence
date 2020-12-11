e3be6243dfee2b6a102ad86cc1eb97b7a6026f4b
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 13:54:35 -0500

    12

--- api/project/router.js
+++ api/project/router.js
@@ -3,33 +3,92 @@ const express = require('express')
 const Project = require('./model')
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    Project.findProjects()
-        .then(projects=>{
+// router.get('/', (req, res)=>{
+//     Project.findProjects()
+//         .then(projects=>{
             
-            projects.map(project=>{
-                project.completed === 0 ? project.completed = false : project.completed = true 
-            })
-            res.status(200).json(projects)
-        })
-        .catch(err=>{
-            res.status(500).json(err.message)
-        })
-})
+//             projects.map(project=>{
+//                 project.completed === 0 ? project.completed = false : project.completed = true 
+//             })
+//             res.status(200).json(projects)
+//         })
+//         .catch(err=>{
+//             res.status(500).json(err.message)
+//         })
+// })
 
-router.post('/',(req, res)=>{
-    if(!req.body.name || !req.body.completed){
-        res.status(400).json({message: `Please fill out name and completed fields`})
-    } else {
-        Project.addProject(req.body)
-            .then(project=>{
-                res.status(201).json(project)
-            })
-            .catch(error=>{
-                res.status(500).json({errorMessage: error.message})
-            })
-    }
+// router.post('/',(req, res)=>{
+//     if(!req.body.name || !req.body.completed){
+//         res.status(400).json({message: `Please fill out name and completed fields`})
+//     } else {
+//         Project.addProject(req.body)
+//             .then(project=>{
+//                 res.status(201).json(project)
+//             })
+//             .catch(error=>{
+//                 res.status(500).json({errorMessage: error.message})
+//             })
+//     }
     
-})
+// })
+const Model = require('./model')
+
+router.get("/", (req, res) => {
+  Model.getAll().then((data) => {
+    const toSend = [];
+    for (let i in data) {
+      if (data[i]["completed"] === 0) {
+        toSend.push({
+          id: data[i].id,
+          name: data[i].name,
+          description: data[i].description,
+          completed: false,
+        });
+      } else {
+        toSend.push({
+          id: data[i].id,
+          name: data[i].name,
+          description: data[i].description,
+          completed: true,
+        });
+      }
+    }
+    res.status(200).json(toSend);
+  });
+});
+
+router.post("/", (req, res) => {
+  Model.create(req.body)
+    // .then((data) => {
+    //   return Model.getById(data);
+    // })
+    // .then((data) => {
+    //   res.status(200).json(data);
+    // });
+    .then((data) => {
+      return Model.getById(data);
+    })
+    .then((data) => {
+      if (data["completed"] === 0) {
+        res.status(200).json({
+          id: data.id,
+          name: data.name,
+          description: data.description,
+          completed: false,
+        });
+      } else {
+        res.status(200).json({
+          id: data.id,
+          name: data.name,
+          description: data.description,
+          completed: true,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
 
 module.exports = router;
--- data/seeds/resources.js
+++ data/seeds/resources.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('resources').insert([
-        {name: 'Resource 1', description:'internet', project_id: 1 },
-        {name: 'Resource 2', description:'books', project_id: 3 },
-        {name: 'Resource 3', description:'school', project_id: 2 }
+        {name: 'Resource 1', description:'internet', /*project_id: 1*/ },
+        {name: 'Resource 2', description:'books', /*project_id: 3*/ },
+        {name: 'Resource 3', description:'school',/* project_id: 2*/ }
       ]);
     });
 };
