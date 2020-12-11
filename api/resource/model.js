01a2ce39a0eb43e84d31be1f802719baf282e5ab
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 15:28:07 -0500

    24

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,33 +1,34 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig')
 
-module.exports = {
-   findResources,
-   addResource,
-   getById
-}
-
-function findResources(){
-    return db('resources')
-}
-function addResource(resource){
-    return db('resources')
-        .insert(resource)
-}
-function getById(data){
-    return db("resources")
-        .where('id', id).first()
-}
-
-
 // module.exports = {
-//   getAll() {
-//     return db("resources");
-//   },
-//   getById(id) {
-//     return db("resources").where("id", id).first();
-//   },
-//   create(data) {
-//     return db("resources").insert(data);
-//   },
-// };
+//    findResources,
+//    addResource,
+//    getById
+// }
+
+// function findResources(){
+//     return db('resources')
+// }
+// function addResource(resource){
+//     return db('resources')
+//         .insert(resource)
+// }
+// function getById(data){
+//     return db("resources")
+//         .where('id', id).first()
+// }
+
+
+module.exports = {
+  getAll() {
+    return db("resources");
+  },
+  getById(id) {
+    return db("resources").where("id", id).first();
+  },
+  create(data) {
+    return db("resources").insert(data);
+  },
+};
+
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,34 +1,34 @@
 // build your `/api/resources` router here
 const express = require('express')
 
-const Resource = require('./model')
+// const Resource = require('./model')
 
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    Resource.findResources()
-        .then(resources=>{
-            res.status(200).json(resources)
-        })
-        .catch(err=>{
-            res.status(500).json(err.message)
-        })
-})
+// router.get('/', (req, res)=>{
+//     Resource.findResources()
+//         .then(resources=>{
+//             res.status(200).json(resources)
+//         })
+//         .catch(err=>{
+//             res.status(500).json(err.message)
+//         })
+// })
 
-router.post('/',(req, res)=>{
-    if(!req.body.name){
-        res.status(400).json({message: `Please fill out name fields`})
-    } else {
-        Resource.addResource(req.body)
-            .then(resource=>{
-                res.status(201).json(resource)
-            })
-            .catch(error=>{
-                res.status(500).json({errorMessage: error.message})
-            })
-    }
+// router.post('/',(req, res)=>{
+//     if(!req.body.name){
+//         res.status(400).json({message: `Please fill out name fields`})
+//     } else {
+//         Resource.addResource(req.body)
+//             .then(resource=>{
+//                 res.status(201).json(resource)
+//             })
+//             .catch(error=>{
+//                 res.status(500).json({errorMessage: error.message})
+//             })
+//     }
     
-})
+// })
 // router.post('/',(req, res)=>{
     
 //         Resource.addResource(req.body)
@@ -57,5 +57,26 @@ router.post('/',(req, res)=>{
 //       res.status(400).json(err.message);
 //     });
 // });
+
+const Model = require("./model");
+
+router.get("/", (req, res) => {
+  Model.getAll().then((data) => {
+    res.status(200).json(data);
+  });
+});
+
+router.post("/", (req, res) => {
+  Model.create(req.body)
+    .then((data) => {
+      return Model.getById(data);
+    })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
 
 module.exports = router;
