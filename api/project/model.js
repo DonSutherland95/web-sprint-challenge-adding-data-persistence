157d2bc717895c38f77efe983f981b75b62934ea
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 14:19:47 -0500

    13

--- api/project/model.js
+++ api/project/model.js
@@ -1,28 +1,28 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig')
 
-// module.exports = {
-//   findProjects,
-//   addProject
-// }
-
-// function findProjects(){
-//     return db('projects')
-// }
-
-// function addProject(project){
-//     return db('projects')
-//     .insert(project)
-// }
-
 module.exports = {
-  getAll() {
-    return db("projects");
-  },
-  getById(id) {
-    return db("projects").where("id", id).first();
-  },
-  create(data) {
-    return db("projects").insert(data);
-  },
-};
+  findProjects,
+  addProject
+}
+
+function findProjects(){
+    return db('projects')
+}
+
+function addProject(project){
+    return db('projects')
+    .insert(project)
+}
+
+// module.exports = {
+//   getAll() {
+//     return db("projects");
+//   },
+//   getById(id) {
+//     return db("projects").where("id", id).first();
+//   },
+//   create(data) {
+//     return db("projects").insert(data);
+//   },
+// };
--- api/project/router.js
+++ api/project/router.js
@@ -3,13 +3,57 @@ const express = require('express')
 const Project = require('./model')
 const router = express.Router();
 
+router.get('/', (req, res)=>{
+    Project.findProjects()
+        .then(projects=>{
+            
+            projects.map(project=>{
+                project.completed === 0 ? project.completed = false : project.completed = true 
+            })
+            res.status(200).json(project)
+        })
+        .catch(err=>{
+            res.status(500).json(err.message)
+        })
+})
+
+router.post('/',(req, res)=>{
+    if(!req.body.name || !req.body.completed){
+        res.status(400).json({message: `Please fill out name and completed fields`})
+    } else {
+        Project.addProject(req.body)
+            .then(project=>{
+                res.status(201).json(project)
+            })
+            .catch(error=>{
+                res.status(500).json({errorMessage: error.message})
+            })
+    }
+    
+})
+
 // router.get('/', (req, res)=>{
 //     Project.findProjects()
 //         .then(projects=>{
-            
+//             const list = [];
 //             projects.map(project=>{
-//                 project.completed === 0 ? project.completed = false : project.completed = true 
+//                 if(project.completed ===0){
+//                     list.push({
+//                         id: project.id,
+//                         name: project.name,
+//                         description: project.description,
+//                         completed: false
+//                     });
+//                 } else{
+//                     list.push({
+//                         id: project.id,
+//                         name: project.name,
+//                         description: project.description,
+//                         completed: true
+//                     });
+//                 }
 //             })
+            
 //             res.status(200).json(projects)
 //         })
 //         .catch(err=>{
@@ -31,64 +75,60 @@ const router = express.Router();
 //     }
     
 // })
-const Model = require('./model')
 
-router.get("/", (req, res) => {
-  Model.getAll().then((data) => {
-    const toSend = [];
-    for (let i in data) {
-      if (data[i]["completed"] === 0) {
-        toSend.push({
-          id: data[i].id,
-          name: data[i].name,
-          description: data[i].description,
-          completed: false,
-        });
-      } else {
-        toSend.push({
-          id: data[i].id,
-          name: data[i].name,
-          description: data[i].description,
-          completed: true,
-        });
-      }
-    }
-    res.status(200).json(toSend);
-  });
-});
+// const Model = require('./model')
 
-router.post("/", (req, res) => {
-  Model.create(req.body)
-    // .then((data) => {
-    //   return Model.getById(data);
-    // })
-    // .then((data) => {
-    //   res.status(200).json(data);
-    // });
-    .then((data) => {
-      return Model.getById(data);
-    })
-    .then((data) => {
-      if (data["completed"] === 0) {
-        res.status(200).json({
-          id: data.id,
-          name: data.name,
-          description: data.description,
-          completed: false,
-        });
-      } else {
-        res.status(200).json({
-          id: data.id,
-          name: data.name,
-          description: data.description,
-          completed: true,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(400).json(err.message);
-    });
-});
+// router.get("/", (req, res) => {
+//   Model.getAll().then((data) => {
+//     const toSend = [];
+//     for (let i in data) {
+//       if (data[i]["completed"] === 0) {
+//         toSend.push({
+//           id: data[i].id,
+//           name: data[i].name,
+//           description: data[i].description,
+//           completed: false,
+//         });
+//       } else {
+//         toSend.push({
+//           id: data[i].id,
+//           name: data[i].name,
+//           description: data[i].description,
+//           completed: true,
+//         });
+//       }
+//     }
+//     res.status(200).json(toSend);
+//   });
+// });
+
+// router.post("/", (req, res) => {
+//   Model.create(req.body)
+    
+//     .then((data) => {
+//       return Model.getById(data);
+//     })
+//     .then((data) => {
+//       if (data["completed"] === 0) {
+//         res.status(200).json({
+//           id: data.id,
+//           name: data.name,
+//           description: data.description,
+//           completed: false,
+//         });
+//       } else {
+//         res.status(200).json({
+//           id: data.id,
+//           name: data.name,
+//           description: data.description,
+//           completed: true,
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err.message);
+//     });
+// });
 
 
 module.exports = router;
