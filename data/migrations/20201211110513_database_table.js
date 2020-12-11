a1ae161aaa387439393353c6929a26320637d7b9
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 12:41:01 -0500

    code grade 6

--- data/migrations/20201211110513_database_table.js
+++ data/migrations/20201211110513_database_table.js
@@ -2,13 +2,13 @@
 exports.up = function(knex) {
   return knex.schema 
     .createTable('projects', table=>{
-        table.increments('id')
+        table.increments('id').unique()
         table.string('name', 128).notNullable()
         table.string('description',128)
         table.boolean('completed').defaultTo(false).notNullable()
     })
     .createTable('resources', table=>{
-        table.increments()
+        table.increments('id').unique()
         table.string('name', 128).notNullable().unique()
         table.string('description',128)
         table.integer('project_id')
@@ -17,7 +17,7 @@ exports.up = function(knex) {
             .onDelete('RESTRICT').onUpdate('RESTRICT')
     })
     .createTable('tasks', table=>{
-        table.increments()
+        table.increments('id').unique()
         table.string('description',128).notNullable()
         table.string('notes', 128)
         table.boolean('completed').defaultTo(false).notNullable()
