1e2f7b2cbfd018850709d701faa49a5a26d38931
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 15:17:24 -0500

    22

--- api/task/model.js
+++ api/task/model.js
@@ -1,18 +1,36 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
+// module.exports = {
+//   findTasks,
+//   addTask
+// }
+
+// function findTasks(){
+//     return db('tasks as t')
+//         .join('projects as p', 't.project_id', 'p.id')
+//         .select('p.name as product_name', 'p.description','t.notes', 't.description ','t.completed')
+// }
+
+// function addTask(task){
+//     return db('tasks')
+//         .insert(task)
+// }
+
 module.exports = {
-  findTasks,
-  addTask
-}
-
-function findTasks(){
-    return db('tasks as t')
-        .join('projects as p', 't.project_id', 'p.id')
-        .select('p.name as product_name', 'p.description','t.notes', 't.description ','t.completed')
-}
-
-function addTask(task){
-    return db('tasks')
-        .insert(task)
-}
+  getAll() {
+    return db("tasks as t")
+      .join("projects as p", "t.project_id", "p.id")
+      .select(
+        "t.*",
+        "p.name as project_name",
+        "p.description as project_description"
+      );
+  },
+  getById(id) {
+    return db("tasks").where("id", id).first();
+  },
+  create(data) {
+    return db("tasks").insert(data);
+  },
+};
--- api/task/router.js
+++ api/task/router.js
@@ -3,18 +3,18 @@ const express = require('express')
 const Task = require('./model')
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    Task.findTasks()
-        .then(tasks=>{
-            // tasks.map(task=>{
-            //     task.completed === 0 ? task.completed = false : task.completed = true 
-            // })
-            res.status(200).json(tasks)
-        })
-        .catch(err=>{
-            res.status(500).json(err.message)
-        })
-})
+// router.get('/', (req, res)=>{
+//     Task.findTasks()
+//         .then(tasks=>{
+//             // tasks.map(task=>{
+//             //     task.completed === 0 ? task.completed = false : task.completed = true 
+//             // })
+//             res.status(200).json(tasks)
+//         })
+//         .catch(err=>{
+//             res.status(500).json(err.message)
+//         })
+// })
 // router.post('/',(req, res)=>{
 //     if(!req.body.description || !req.body.completed){
 //         res.status(400).json({message: `Please fill out description and completed fields`})
@@ -29,16 +29,81 @@ router.get('/', (req, res)=>{
 //     }
     
 // })
-router.post('/',(req, res)=>{
+// router.post('/',(req, res)=>{
     
-        Task.addTask(req.body)
-            .then(task=>{
-                res.status(201).json(task)
-            })
-            .catch(error=>{
-                res.status(500).json({errorMessage: error.message})
-            })
+//         Task.addTask(req.body)
+//             .then(task=>{
+//                 res.status(201).json(task)
+//             })
+//             .catch(error=>{
+//                 res.status(500).json({errorMessage: error.message})
+//             })
     
     
-})
+// })
+const Model = require("./model");
+router.get("/", (req, res) => {
+  const toReturn = [];
+  Model.getAll().then((data) => {
+    for (let i in data) {
+      if (data[i].completed === 1) {
+        toReturn.push({
+          id: data[i].id,
+          project_id: data[i].project_id,
+          description: data[i].description,
+          notes: data[i].notes,
+          completed: true,
+          project_name: data[i].project_name,
+          project_description: data[i].project_description,
+        });
+      } else {
+        toReturn.push({
+          id: data[i].id,
+          project_id: data[i].project_id,
+          description: data[i].description,
+          notes: data[i].notes,
+          completed: false,
+          project_name: data[i].project_name,
+          project_description: data[i].project_description,
+        });
+      }
+    }
+    res.status(200).json(toReturn);
+  });
+});
+
+router.post("/", (req, res) => {
+  Model.create(req.body)
+    .then((data) => {
+      return Model.getById(data);
+    })
+    .then((data) => {
+      Model.getById(data);
+      if (data.completed === 1) {
+        res.status(200).json({
+          id: data.id,
+          project_id: data.project_id,
+          description: data.description,
+          notes: data.notes,
+          completed: true,
+          project_name: data.project_name,
+          project_description: data.project_description,
+        });
+      } else {
+        res.status(200).json({
+          id: data.id,
+          project_id: data.project_id,
+          description: data.description,
+          notes: data.notes,
+          completed: false,
+          project_name: data.project_name,
+          project_description: data.project_description,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
 module.exports = router;
