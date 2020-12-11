10539ec6468fa20f7439949c14727bc78d4a952a
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-11 14:27:19 -0500

    15

--- api/project/router.js
+++ api/project/router.js
@@ -3,6 +3,34 @@ const express = require('express')
 const Project = require('./model')
 const router = express.Router();
 
+// router.get('/', (req, res)=>{
+//     Project.findProjects()
+//         .then(projects=>{
+            
+//             projects.map(project=>{
+//                 project.completed === 0 ? project.completed = false : project.completed = true 
+//             })
+//             res.status(200).json(projects)
+//         })
+//         .catch(err=>{
+//             res.status(500).json(err.message)
+//         })
+// })
+
+// router.post('/',(req, res)=>{
+//     if(!req.body.name || !req.body.completed){
+//         res.status(400).json({message: `Please fill out name and completed fields`})
+//     } else {
+//         Project.addProject(req.body)
+//             .then(project=>{
+//                 res.status(201).json(project)
+//             })
+//             .catch(error=>{
+//                 res.status(500).json({errorMessage: error.message})
+//             })
+//     }
+    
+// })
 router.get('/', (req, res)=>{
     Project.findProjects()
         .then(projects=>{
@@ -22,8 +50,11 @@ router.post('/',(req, res)=>{
         res.status(400).json({message: `Please fill out name and completed fields`})
     } else {
         Project.addProject(req.body)
-            .then(project=>{
-                res.status(201).json(project)
+            .then(projects=>{
+                projects.map(project=>{
+                    project.completed === 0 ? project.completed = false : project.completed = true 
+                })
+                res.status(201).json(projects)
             })
             .catch(error=>{
                 res.status(500).json({errorMessage: error.message})
